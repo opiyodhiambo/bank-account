1ad7f68fb9c1474f4cd4327af06829ce0f7817cb
opiyodhiambo <rnldlinus@gmail.com>
2023-08-20 10:49:06 +0300

    Initial commit

---
+++ src/main/scala/adventure/BankAccount.scala
@@ -0,0 +1,12 @@
+package adventure
+
+object BankAccount {
+  case class Deposit(amount: BigInt) {
+    require(amount > 0)
+  }
+  case class Withdraw(amount: BigInt) {
+    require(amount > 0)
+  }
+  case object Done
+  case object Failed
+}
